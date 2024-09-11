cfda84c36982e44d966749c84869d189cf818ddf
bayjanov.bakh <bayjanov.bakh@sepconsulting.hu>
2024-09-12 00:33:55 +0200

    Read-only and optiononal properties

--- 4. Classes, Interfaces and OOP/src/index.ts
+++ 4. Classes, Interfaces and OOP/src/index.ts
@@ -1,8 +1,9 @@
 // Classes
 class Account { 
-    id: number;
+    readonly id: number; // now that we have readonly property, we can't change the value of id
     owner: string;
     balance: number;
+    nickname?: string; // Optional property
 
     constructor(id: number, owner: string, balance: number) {
         this.id = id;
@@ -22,14 +23,12 @@ class Account {
 // Creating Objects
 
 let account = new Account(1, "John", 0);
-account.deposit(100);
-console.log(account.balance); // 500
-console.log(account); // Account { id: 1, owner: 'John', balance: 500 }
-console.log(account instanceof Account); // true
+// account.id = 2; // Error: Cannot assign to 'id' because it is a read-only property.
 
+console.log(account instanceof Account); // true
 // Union  
-// if (typeof someObj === 'number') {}    ->  
-// if you are using a type guard to narrow down the type and you are dealing with a custom object, you should always use the instanceof operator, not the typeof operator. This is because the typeof operator will always return 'object' for custom objects, regardless of their actual type, while the instanceof operator will return the correct type, like Account in this case.
+
+// if (typeof someObj === 'number') {}    ->  // if you are using a type guard to narrow down the type and you are dealing with a custom object, you should always use the instanceof operator, not the typeof operator. This is because the typeof operator will always return 'object' for custom objects, regardless of their actual type, while the instanceof operator will return the correct type, like Account in this case.
 
 
 
