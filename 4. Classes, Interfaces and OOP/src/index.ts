39654f513084a37afac02bb17db82b1016ce3af0
bayjanov.bakh <bayjanov.bakh@sepconsulting.hu>
2024-09-12 00:43:02 +0200

    Access modiefiers: public & private

--- 4. Classes, Interfaces and OOP/src/index.ts
+++ 4. Classes, Interfaces and OOP/src/index.ts
@@ -2,20 +2,29 @@
 class Account { 
     readonly id: number; // now that we have readonly property, we can't change the value of id
     owner: string;
-    balance: number;
+    private _balance: number; // Now balance is private, so we can't access it outside the class
     nickname?: string; // Optional property
 
     constructor(id: number, owner: string, balance: number) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
 
     deposit(amount: number): void {
         if (amount <= 0) {
             throw new Error("Invalid amount!");
         }
-        this.balance += amount;
+        // Record a transaction
+        this._balance += amount;
+    }
+
+    private calculateTax(): number {
+        return this._balance * 0.25;
+    }
+
+    getBalance(): number { 
+        return this._balance;
     }
 }
 
@@ -23,13 +32,11 @@ class Account {
 // Creating Objects
 
 let account = new Account(1, "John", 0);
-// account.id = 2; // Error: Cannot assign to 'id' because it is a read-only property.
-
-console.log(account instanceof Account); // true
-// Union  
-
-// if (typeof someObj === 'number') {}    ->  // if you are using a type guard to narrow down the type and you are dealing with a custom object, you should always use the instanceof operator, not the typeof operator. This is because the typeof operator will always return 'object' for custom objects, regardless of their actual type, while the instanceof operator will return the correct type, like Account in this case.
 
 
+// Access Modifiers
+// Public, Private and Protected
+console.log(account.getBalance()); 
+// account.balance; // Error: Property 'balance' is private and only accessible within class 'Account'
 
 
