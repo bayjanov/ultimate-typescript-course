997c80306090ba85862bde198d88b02ef165240b
bayjanov.bakh <bayjanov.bakh@sepconsulting.hu>
2024-09-12 01:02:57 +0200

    Getters and setters

--- 4. Classes, Interfaces and OOP/src/index.ts
+++ 4. Classes, Interfaces and OOP/src/index.ts
@@ -22,9 +22,18 @@ class Account {
         return this._balance * 0.25;
     }
 
-    getBalance(): number { 
+    // Getter   -   This is a read-only property, accessible just like a field
+    get balance(): number { 
         return this._balance;
     }
+
+//     // Setter   -   This is our chance to validate the value before setting it
+//     set balance(value: number) { 
+//         if (value < 0) {
+//             throw new Error("Invalid balance!");
+//         }
+//         this._balance = value;
+//     }
 }
 
 
@@ -32,10 +41,10 @@ class Account {
 
 let account = new Account(1, "John", 0);
 
-
 // Access Modifiers
 // Public, Private and Protected
-console.log(account.getBalance()); 
+console.log(account.balance); 
+// account.balance = 100; // Setter is called
 // account.balance; // Error: Property 'balance' is private and only accessible within class 'Account'
 
 
