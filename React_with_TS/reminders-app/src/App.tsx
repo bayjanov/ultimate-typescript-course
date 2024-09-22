d96523b9b7d78b6fb72855994bbd5f4db44d0721
bayjanov.bakh <bayjanov.bakh@sepconsulting.hu>
2024-09-22 13:55:23 +0200

    useState

--- React_with_TS/reminders-app/src/App.tsx
+++ React_with_TS/reminders-app/src/App.tsx
@@ -1,17 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import ReminderList from './components/ReminderList';
-import Reminder from './models/reminder';
- 
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ReminderList from "./components/ReminderList";
+import Reminder from "./models/reminder";
 
-const reminders: Reminder[] = [
-  { id: 1, title: 'Buy Milk' },
-  { id: 2, title: 'Call Mom' },
-  { id: 3, title: 'Send Report' }
-];
+// const reminders: Reminder[] = [
+//   { id: 1, title: 'Buy Milk' },
+//   { id: 2, title: 'Call Mom' },
+//   { id: 3, title: 'Send Report' }
+// ];
 
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([
+    { id: 1, title: "Buy Milk" },
+    { id: 2, title: "Call Mom" },
+    { id: 3, title: "Send Report" },
+  ]);
+
   return (
     <div className="App">
       <ReminderList items={reminders} />
