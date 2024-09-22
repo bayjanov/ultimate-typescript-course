3f15fc17192a08d8e9a2ca6c05d2e08011b1fce6
bayjanov.bakh <bayjanov.bakh@sepconsulting.hu>
2024-09-22 12:33:24 +0200

    deleting the previous folder

--- JS_Integrations/newTax.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function newTaxCalculation(income: number): number;  // We are just declaring the fuction signature here. The implementation is in target file - newTax.js
-
-// export declare function sayHello(): void;  // We are just declaring the fuction signature here. The implementation is in target file - newTax.js
--- JS_Integrations/tax.js
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck
-// The above line is to ignore the typescript error
-function calculateTax(income) {
-    return income * .3;
-}
-
-module.exports = { calculateTax };
